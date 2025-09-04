3130c717f42fa2aa07327241cd99b7178c9fa786
Ayush Shukla <nitianayush4141@gmail.com>
2025-09-04 14:34:10 +0530

    initial commit

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,13 @@
+a=int(input("Enter first number: "))
+print(a)
+b=int(input("Enter second number: "))
+print(b)
+
+#if (a>b):
+#    print("a is greater than b")
+#elif (b>a):
+#    print("b is greater than a")
+#else:
+#    print("a is equal to b")   
+
+print("a") if a>b else print("b")
